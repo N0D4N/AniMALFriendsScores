151d149624d2c467cb2044e0ad85672dacfb1a05
N0D4N <50947930+N0D4N@users.noreply.github.com>
2022-04-12 20:16:18 +0300

    Properly visualise link to MAL page. Minor refactoring.

--- src/anilist.content.js
+++ src/anilist.content.js
@@ -1,9 +1,12 @@
 const extensionUniqueIdForCustomFollowingDiv = 'made-by-AniMALFriendsScores';
+const malUrl = 'https://myanimelist.net';
+const mal = 'MyAnimeList';
+const cdnMalImages = 'https://cdn.myanimelist.net/images/';
 
 async function process() {
     /// FUNCTIONS
     async function getMalIdFromAlId(alId) {
-        console.log('Trying to get Mal id for this entry')
+        console.log('Trying to get Mal id for this entry');
         return fetch('https://graphql.anilist.co', {
             method: 'post',
             headers: {
@@ -20,6 +23,10 @@ async function process() {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
+    function getMediaUrl(type, id) {
+        return `${malUrl}/${type}/${id}`;
+    }
+
     const getMediaType = () => window.location.href.includes('/anime/') ? 'anime' : 'manga';
 
     async function malGetFullUrl(type, id) {
@@ -33,7 +40,7 @@ async function process() {
         }
         console.log('Failed to load full url from jikan.moe, trying to load it from MAL');
         return new Promise((resolve, reject) => {
-            chrome.runtime.sendMessage({'fullUrl': `https://myanimelist.net/${type}/${id}`}, function (response) {
+            chrome.runtime.sendMessage({'fullUrl': getMediaUrl(type, id)}, function (response) {
                 if (response.fullUrl)
                     resolve(response.fullUrl);
                 else
@@ -83,111 +90,116 @@ async function process() {
         });
     }
 
+    function getDataAttributeName(element) {
+        for (let attribute of element.attributes) {
+            if (attribute.name.startsWith('data-'))
+                return attribute.name;
+        }
+        throw "Couldn't get value of data attribute";
+    }
+
     function createLinkToMal(type, id) {
         const isMobile = document.getElementsByClassName('mobile-nav').length > 0;
         const externalLinks = document.getElementsByClassName('external-links-wrap');
         if (externalLinks.length > 0) {
             const externalLinksDiv = externalLinks[1] ? externalLinks[1] : externalLinks[0];
-            let dataAttributeName;
-            for (let attribute of externalLinksDiv.children[1].attributes) {
-                if (attribute.name.startsWith('data-')) {
-                    dataAttributeName = attribute.name;
-                    break;
-                }
-            }
+            const dataAttributeName = getDataAttributeName(externalLinksDiv.children[0]);
             const a = document.createElement('a');
             a.className = 'external-link';
-            a.href = `https://myanimelist.net/${type}/${id}`;
-            a.text = 'MyAnimeList';
+            a.href = getMediaUrl(type, id);
             a.setAttribute(dataAttributeName, '');
+            a.style.cssText = '--link-color: #2e51a2;';
+            const div = document.createElement('div');
+            div.className = 'icon-wrap';
+            div.setAttribute(dataAttributeName, '');
+            const img = document.createElement('img');
+            img.className = 'icon';
+            img.setAttribute(dataAttributeName, '');
+            img.src = `${cdnMalImages}favicon.ico`;
+            img.style.minWidth = '24px';
+            div.appendChild(img);
+            const span = document.createElement('span');
+            span.className = 'name';
+            span.setAttribute(dataAttributeName, '');
+            span.textContent = mal;
+            a.appendChild(div);
+            a.appendChild(span);
             externalLinksDiv.appendChild(a);
         } else {
             const sidebar = document.getElementsByClassName('sidebar');
-            let dataAttributeName;
-            for (let attribute of sidebar[0].children[sidebar[0].children.length - 1].attributes) {
-                if (attribute.name.startsWith('data-')) {
-                    dataAttributeName = attribute.name;
-                    break;
-                }
-            }
+            const dataAttributeName = getDataAttributeName(sidebar[0].children[sidebar[0].children.length - 1]);
             const a = document.createElement('a');
             a.className = 'button';
-            a.href = `https://myanimelist.net/${type}/${id}`;
-            a.text = 'MyAnimeList';
+            a.href = getMediaUrl(type, id);
+            a.text = mal;
             a.style.cssText = 'display: flex;';
             a.setAttribute(dataAttributeName, '');
-            if(isMobile){
+            if (isMobile) {
                 document.getElementsByClassName('overview')[0].appendChild(a);
-            }
-            else{
+            } else {
                 sidebar[0].appendChild(a);
             }
         }
     }
 
-    function createFriendStatDiv(hrefUrl, nickname, avatarUrl, statusMessage, score){
-		const parentDiv = document.createElement('div');
-		parentDiv.className = 'limit';
-		const a = document.createElement('a');
-		parentDiv.appendChild(a);
-		a.className = 'follow';
-		a.href = hrefUrl;
-		a.style.cssText = 'align-items:center; background:rgb(var(--color-foreground)); border-radius:3px; display:grid; font-size:1.4rem; grid-template-columns:30px 2fr 1fr .5fr; margin-bottom:10px; padding:10px; border-style: solid; border-width: 1px;';
-		const avatarDiv = document.createElement('div');
-		a.appendChild(avatarDiv);
-		avatarDiv.className = 'avatar';
-		avatarDiv.style.cssText = 'background-position:50%; background-repeat:no-repeat; background-size:cover; border-radius:3px; height:30px; width:30px; background-image: ' + avatarUrl;
+    function createFriendStatDiv(hrefUrl, nickname, avatarUrl, statusMessage, score) {
+        const parentDiv = document.createElement('div');
+        parentDiv.className = 'limit';
+        const a = document.createElement('a');
+        parentDiv.appendChild(a);
+        a.className = 'follow';
+        a.href = hrefUrl;
+        a.style.cssText = 'align-items:center; background:rgb(var(--color-foreground)); border-radius:3px; display:grid; font-size:1.4rem; grid-template-columns:30px 2fr 1fr .5fr; margin-bottom:10px; padding:10px; border-style: solid; border-width: 1px;';
+        const avatarDiv = document.createElement('div');
+        a.appendChild(avatarDiv);
+        avatarDiv.className = 'avatar';
+        avatarDiv.style.cssText = 'background-position:50%; background-repeat:no-repeat; background-size:cover; border-radius:3px; height:30px; width:30px; background-image: ' + avatarUrl;
         const nameDiv = document.createElement('div');
         a.appendChild(nameDiv);
         nameDiv.className = 'name';
-		nameDiv.innerText = nickname;
-        nameDiv.style.cssText = 'padding-left:15px; font-weight:500;';
+        nameDiv.innerText = nickname;
+        nameDiv.style.cssText = 'padding-left:15px; font-weight:500;overflow: hidden;margin-right:2em;';
         const statusDiv = document.createElement('div');
         a.appendChild(statusDiv);
         statusDiv.className = 'status';
         statusDiv.innerText = statusMessage;
-		const scoreSpan = document.createElement('span');
+        const scoreSpan = document.createElement('span');
         a.appendChild(scoreSpan);
         scoreSpan.innerText = score;
-		return parentDiv;
+        return parentDiv;
     }
 
     function displayFriendsStatistics(friendsStats, isLoggedIn) {
         const followingDiv = getOrCreateFollowingDiv();
-		if(isLoggedIn){
-			friendsStats.forEach(fs => {
-				followingDiv.appendChild(createFriendStatDiv(fs.profileUrl, fs.nickname, fs.avatar, fs.status, fs.score));
-			});
-		}
-        else{
-			followingDiv.appendChild(createFriendStatDiv('https://myanimelist.net/login.php', 'You should be logged in to MAL to use AniMALFriendsScores extension',
-														 'url("https://cdn.myanimelist.net/images/MalAppIcon_180px.png")', 'Click to log in', ''));
-		}
+        if (isLoggedIn) {
+            friendsStats.forEach(fs => {
+                followingDiv.appendChild(createFriendStatDiv(fs.profileUrl, fs.nickname, fs.avatar, fs.status, fs.score));
+            });
+        } else {
+            followingDiv.appendChild(createFriendStatDiv(`${malUrl}/login.php`, 'You should be logged in to MAL to use AniMALFriendsScores extension',
+                `url("${cdnMalImages}MalAppIcon_180px.png")`, 'Click to log in', ''));
+        }
     }
 
     const getAlId = () => window.location.href.match(/\d+/)[0];
     /// END OF FUNCTIONS
+
     let currentUrl = '';
 
     // looping forever so we can react to change of url
     // noinspection InfiniteLoopJS
     while (true) {
-        // Current page is anime or manga page
-        if (window.location.href.match(/(anilist.co\/(manga|anime)\/(\d+)\/([\w-_]+)((\/social)|$|(\/$)))/)
+        if (// Url changed
+            window.location.href !== currentUrl
             &&
-            // Url changed
-            window.location.href !== currentUrl) {
+            // Current page is anime or manga page
+            window.location.href.match(/(anilist.co\/(manga|anime)\/(\d+)\/([\w-_]+)((\/social)|$|(\/$)))/)) {
             currentUrl = window.location.href;
             // Clear all scores from previous pages
-            for (let i = 0; i < 10; i++) { // loop 10 times because clearing once can leave previu
+            for (let i = 0; i < 10; i++) { // loop 10 times because clearing once can leave previous
                 for (let elementsByTagNameElement of document.getElementsByTagName('a')) {
-                    if (elementsByTagNameElement.href.includes('https://myanimelist.net')) {
-                        if(elementsByTagNameElement.href.includes('profile')){
-                            elementsByTagNameElement.parentElement.remove();
-                        }
-                        else {
-                            elementsByTagNameElement.remove();
-                        }
+                    if (elementsByTagNameElement.href.includes(malUrl)) {
+                        elementsByTagNameElement.remove();
                     }
                 }
             }
@@ -198,10 +210,10 @@ async function process() {
 
             const malId = await getMalIdFromAlId(alId);
             if (malId !== null && malId !== undefined) {
-                createLinkToMal(type, malId);
                 const malFullUrl = await malGetFullUrl(type, malId);
                 const response = await getMalTableFriendUpdatesTableOrNull(malFullUrl);
                 displayFriendsStatistics(response.friendsStats, response.loggedIn);
+                createLinkToMal(type, malId);
             }
         }
         await sleep(500);
